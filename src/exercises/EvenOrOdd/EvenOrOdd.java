81332c8df73c2ee00d2fbcaaf81c9a2e74ad4bc3
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-11-20 12:14:02 +0100

    EvenOrOdd

---
+++ src/exercises/EvenOrOdd/EvenOrOdd.java
@@ -0,0 +1,11 @@
+package exercises.EvenOrOdd;
+
+public class EvenOrOdd {
+    public static String IsEvenOrOdd(int x) {
+        if (x % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+}
---
+++ src/exercises/EvenOrOdd/Main.java
@@ -0,0 +1,10 @@
+package exercises.EvenOrOdd;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+
+        String result = EvenOrOdd.IsEvenOrOdd(x);
+        System.out.println("The number is " + result);
+    }
+}
