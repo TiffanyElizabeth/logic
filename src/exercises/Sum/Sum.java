f6840f64e76312c2aa92c7ec2aaf726345ddd2bd
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-11-20 12:15:34 +0100

    Sum

---
+++ src/exercises/Sum/Sum.java
@@ -0,0 +1,14 @@
+package exercises.Sum;
+
+public class Sum {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3, 4 };
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            System.out.println(sum);
+        }
+    }
+
+}
